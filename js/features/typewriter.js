068e16385e1c9cf469dad88a1719df52b4f192db
Nick ODonoghue <nicholasgodonoghue@gmail.com>
2023-03-30 14:46:59 +0100

    added typewriter text to HTML, styled span in CSS, wrote typewriter.js script

---
+++ js/features/typewriter.js
@@ -0,0 +1,13 @@
+const textArray = ["Hi, I'm Nick"];
+let textPosition = 0;
+const speed = 250;
+
+export function typewriter() {
+
+    document.querySelector("#typewriter__text").innerHTML = textArray[0].substring(0, textPosition) + `<span class="blinker">\u25ae</span>`;
+
+    if (textPosition++ != textArray[0].length)
+        setTimeout(typewriter, speed);
+}
+
+window.addEventListener("load", typewriter);
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
 import { hamburger } from "./features/hamburger.js";
-
+import { typewriter } from "./features/typewriter.js";
 hamburger()
+// typewriter()
