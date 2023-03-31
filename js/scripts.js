418f6572db8e97480f40aedc43b48760cc224b89
Nick ODonoghue <nicholasgodonoghue@gmail.com>
2023-03-31 16:29:31 +0100

    added hidden class to tech-stack divs, added animation.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
 import { hamburger } from "./features/hamburger.js";
 import { typewriter } from "./features/typewriter.js";
 import { smoothScroll } from "./features/smoothscroll.js";
+import { animations } from "./features/animations.js";
