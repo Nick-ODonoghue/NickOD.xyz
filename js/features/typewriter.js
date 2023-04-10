50a7ac2943889261fc7619dd50aed8b9169e46d3
Nick ODonoghue <nicholasgodonoghue@gmail.com>
2023-04-10 11:01:26 +0100

    added notes to typewriter.js explaining what the function typewriter does

--- js/features/typewriter.js
+++ js/features/typewriter.js
@@ -1,13 +1,22 @@
+// Create an array with only 1 value, a string
 const textArray = ["Hi, I'm Nick"];
+// Initialize "textPosition" and sets it zero
 let textPosition = 0;
-const speed = 100;
+// Initialize "speed" and set it to 125ms
+const speed = 125;
 
 export function typewriter() {
 
-    document.querySelector("#typewriter__text").innerHTML = textArray[0].substring(0, textPosition) + `<span class="blinker">\u25ae</span>`;
+    // Query the DOM for the element with the ID "typewriter__text" and set its innerHTML property to the first element in the "textArray" array up to the current "textPosition"
+    document.querySelector("#typewriter__text").innerHTML = textArray[0].substring(0,
+        // Add a span element with a class of "blinker" to simulate a blinking cursor
+        textPosition) + `<span class="blinker">\u25ae</span>`;
 
+    // Check if "textPosition" is not equal to the length of the first element in the "textArray" array
     if (textPosition++ != textArray[0].length)
+        // If true and it is not equal, then setTimeout calling typewriter function again adding the "speed" variable for the setTimeout delay
         setTimeout(typewriter, speed);
 }
 
+// Set an event listener on the window object to listen for the load event and call the typewriter function
 window.addEventListener("load", typewriter);
