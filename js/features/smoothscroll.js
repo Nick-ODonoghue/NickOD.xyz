00a09e4d40fa2a0ee832e81fcffbc57deb6f17ba
Nick ODonoghue <nicholasgodonoghue@gmail.com>
2023-04-10 10:46:33 +0100

    added notes to smoothscroll.js explaining what the function smoothScoll does

--- js/features/smoothscroll.js
+++ js/features/smoothscroll.js
@@ -1,7 +1,12 @@
 export function smoothScroll() {
+    // Grab the element with a class of "header"
     const header = document.querySelector(".header");
+    // Get the offsetHeight property of the header element
     const headerHeight = header.offsetHeight;
 
+    // Set a CSS custom property called "--scroll-padding" on the document element
+    // Set the value equal to the headerHeight plus 10 pixels
+    // Regardless of the height of the navbar nothing will ever be hidden behind it when smoothScroll is used
     document.documentElement.style.setProperty("--scroll-padding", headerHeight + 10 + "px")
 }
 
