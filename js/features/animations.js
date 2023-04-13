37b59f8c9f929c29fcdda6503b12b7019db2c1b2
Nick ODonoghue <nicholasgodonoghue@gmail.com>
2023-04-13 12:48:13 +0100

    added scroll-down arrow indicator and styled

--- js/features/animations.js
+++ js/features/animations.js
@@ -1,20 +1,27 @@
 export function animationsTech() {
+
     // Creates instance of IntersectionObserver and pass it a callback as a parameter
     const observer = new IntersectionObserver((entries) => {
+
         // Loop through each entry in the array passed to the callback function
         entries.forEach((entry) => {
+
             // Check if the entry is currently intersecting with the viewport
             if (entry.isIntersecting) {
+
                 // Add CSS "show" to the target element of the entry
                 entry.target.classList.add("show");
             } else {
+
                 // Remove CSS "show" from the target element of the entry
                 entry.target.classList.remove("show");
             }
         });
     });
+
     // Grab all elements with a class of "hidden" from the DOM
     const hiddentElements = document.querySelectorAll(".hidden");
+
     // Loop through each "hidden" element and attach the IntersectionObserver instance to each of them
     hiddentElements.forEach((el) => observer.observe(el));
 }
